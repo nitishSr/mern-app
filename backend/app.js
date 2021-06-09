350eea9f6c846729af1669d2d4f712e815338626
nitishSr <nsrivast@redhat.com>
2021-06-09 16:51:02 +0530

    Changes for deploying app on heroku

--- backend/app.js
+++ backend/app.js
@@ -41,3 +41,11 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+  
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../build')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html;
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../build/index.html'))
+})
